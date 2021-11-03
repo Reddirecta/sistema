a39b84a81f8bb2241eee2d717544539fd65b4e8f
unknown <lcflores@hotmail.com>
2021-11-02 21:24:26 -0700

    Sistema dos

---
+++ info.php
@@ -0,0 +1,11 @@
+<?php 
+//echo $_SERVER['DOCUMENT_ROOT'];
+phpinfo();
+//$msg = "First line of text\nSecond line of text";
+
+ // use wordwrap() if lines are longer than 70 characters
+// $msg = wordwrap($msg,70);
+
+ // send email
+// mail("lcflores2000@gmail.com","My subject",$msg);
+?>
